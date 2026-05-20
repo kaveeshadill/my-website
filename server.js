434a39d99d17cd79c9a6e308b1f564003606cc82
kaveeshadill <r222pm5chh@privaterelay.appleid.com>
2026-05-20 14:50:22 +0530

    fix render

--- server.js
+++ server.js
@@ -131,6 +131,6 @@ app.post('/api/upload', requireAdmin, upload.single('file'), (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`\nLocal website: http://localhost:${PORT}/index.html`);
-  console.log(`Admin panel:   http://localhost:${PORT}/admin.html`);
+  console.log(`Admin panel:    http://localhost:${PORT}/admin.html`);
   console.log(`Login: username=${ADMIN_USERNAME} password=${ADMIN_PASSWORD}\n`);
 });
